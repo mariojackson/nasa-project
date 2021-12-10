b65f9bcd09db3f3496f7ff6dac66188a0ff6b6f2
Mario Jackson <mario@jackson.softwre>
2021-12-10 16:44:19 +0100

    Add mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,24 @@
 import http from 'http';
+import mongoose from 'mongoose';
 
 import app from './app.js';
 import { loadPlanetsData } from './models/planets.model.js';
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = 'mongodb://127.0.0.1:27017/nasa?retryWrites=true';
+
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+  console.log('Connected successfully to MongoDB');
+});
+
+mongoose.connection.on('error', (error) => {
+  console.error(error);
+});
+
+await mongoose.connect(MONGO_URL);
 await loadPlanetsData();
 
 server.listen(PORT, () => {
